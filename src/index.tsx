5201ecf1afc6e115e2ab4437a9570474d047b8f8
Brijunas <32673235+Brijunas@users.noreply.github.com>
2023-02-16 01:17:09 +0200

    linted whole project

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { store } from './app/store'
 import App from './App'
 import './index.css'
 
-const container = document.getElementById('root')!
+const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 
 root.render(
