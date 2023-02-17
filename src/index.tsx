5cdb3ded291db4af68f06e65b0efcfde1b20ac92
Brijunas <32673235+Brijunas@users.noreply.github.com>
2023-02-17 14:49:02 +0200

    installed font

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 
