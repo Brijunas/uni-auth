b23536c01f0a684f596b4c961abbc552a4e6a92c
Brijunas <32673235+Brijunas@users.noreply.github.com>
2023-02-17 15:52:34 +0200

    grid & button sign in

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Button } from '@mui/material'
+import StyledGrid from './components/StyledGrid'
 
-const App: React.FC = () => <></>
+const App: React.FC = () => (
+  <StyledGrid container direction='column' justifyContent='center' alignItems='center'>
+    <Button variant='contained'>Sign In</Button>
+  </StyledGrid>
+)
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
 
+import './index.css'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
