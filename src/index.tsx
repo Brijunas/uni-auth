7c899a4cfe8e584b35aba333d940ca9db8a85889
Brijunas <32673235+Brijunas@users.noreply.github.com>
2023-05-05 17:13:08 +0300

    added typescript strict plugin

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,12 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
-const container = document.getElementById('root') as HTMLElement
+const container = document.getElementById('root')
+
+if (!container) {
+  throw new Error('Root element not found')
+}
+
 const root = createRoot(container)
 
 root.render(
